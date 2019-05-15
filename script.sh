b57b29739433d7a504775a3241d86c7f7c11df8f
Juan Camilo Agudelo Marin <50254391+juagudelom@users.noreply.github.com>
2019-05-14 23:11:50 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,7 @@
 # Escriba su código aquí
+sed 's/\([0-9][0-9]\)\/\([0-9][0-9]\)\/\([0-9][0-9]\)/20\3-\2-\1/' data.csv > out.1
+sed 's/;N/;\N/g' out.1 > out.2
+sed 's/,/./g' out.2 > out.3
+sed 's/;/,/g' out.3 > out.4
+sed -e'/N/d' out.4 > lab-01-juagudelom.csv
+rm out*
